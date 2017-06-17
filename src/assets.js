d76b127e8767b058f750f0d6fc747d24dafdf677
Jonas Auer <jonas.auer.94@gmail.com>
2017-06-17 11:58:26 +0200

    Add support for sound playback

--- src/assets.js
+++ src/assets.js
@@ -1,16 +1,20 @@
 'use strict'
 
-define(['lib/pixi.min'],
-       ( PIXI ) => {
+define(() => {
   return { load: (loadingDone) => {
-    const { loader } = PIXI
+    // const { loader } = PIXI
 
-    loader.add([
+    PIXI.loader.add([
       { name: 'placeholder-1', url: 'img/placeholder-1.png' },
       { name: 'placeholder-2', url: 'img/placeholder-2.png' },
       { name: 'placeholder-3', url: 'img/placeholder-3.png' },
       { name: 'placeholder-4', url: 'img/placeholder-4.png' },
-      { name: 'walk-cycle-sprites', url: 'img/antonius-walkcycle.png'}
+      { name: 'walk-cycle-sprites', url: 'img/antonius-walkcycle.png'},
+      { name: 'gans-001', url: 'aud/Gans-001.wav' },
+      { name: 'gans-002', url: 'aud/Gans-002.wav' },
+      { name: 'gans-003', url: 'aud/Gans-003.wav' },
+      { name: 'gans-004', url: 'aud/Gans-004.wav' },
+      { name: 'gans-005', url: 'aud/Gans-005.wav' }
     ])
     .on('progress', onProgress)
     .load(doneLoading)
@@ -21,7 +25,7 @@ define(['lib/pixi.min'],
     }
 
     function loadAnimation(textureName, width, height) {
-      const spriteSheet = loader.resources[textureName].texture.baseTexture
+      const spriteSheet = PIXI.loader.resources[textureName].texture.baseTexture
       let frames = []
       for (let i = 0; i < spriteSheet.width - width; i += width) {
         const frameTexture = new PIXI.Texture(spriteSheet)
@@ -32,7 +36,7 @@ define(['lib/pixi.min'],
     }
 
     function doneLoading() {
-      loader.resources['walk-cycle'] = loadAnimation('walk-cycle-sprites', 32, 32)
+      PIXI.loader.resources['walk-cycle'] = loadAnimation('walk-cycle-sprites', 32, 32)
 
       console.log('loading done')
       loadingDone()
--- src/sound.js
+++ src/sound.js
@@ -1,17 +1,18 @@
 'use strict'
 
-define(['lib/pixi.min'], (PIXI) => {
+define(['lib/pixi.min', 'lib/pixi-sound.min'], (PIXI, { default: sound }) => {
+  
 
-  var sound = new Howl({
-    src: ['sound/gost.mp3'],
-    sprite: {
-      ghost_1: [3000, 3500],
-      ghost_2: [3500, 4000],
-      ghost_3: [4000, 4500]
-    }
-  });
-
-  sound.play('ghost_3')
+  // var sound = new Howl({
+  //   src: ['sound/gost.mp3'],
+  //   sprite: {
+  //     ghost_1: [3000, 3500],
+  //     ghost_2: [3500, 4000],
+  //     ghost_3: [4000, 4500]
+  //   }
+  // });
+  //
+  // sound.play('ghost_3')
 
   // playrandom('sound', 4, 3)
   //
